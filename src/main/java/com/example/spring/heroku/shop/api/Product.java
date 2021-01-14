00cff208672ca11c1a6c62c94bcd1de7485e113c
depalsap <76130057+depalsap@users.noreply.github.com>
2021-01-14 13:24:11 +0100

    Add files via upload

---
+++ src/main/java/com/example/spring/heroku/shop/api/Product.java
@@ -0,0 +1,61 @@
+package com.example.spring.heroku.shop.api;
+
+public class Product {
+
+    private String pId;
+    private String pName;
+    private String pDescript;
+    private String pCategory;
+    private long netPrice;
+
+    public Product() {}
+
+    public Product(String pId, String pName, String pDescript, String pCategory, long netPrice) {
+        this.pId = pId;
+        this.pName = pName;
+        this.pDescript = pDescript;
+        this.pCategory = pCategory;
+        this.netPrice = netPrice;
+    }
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpDescript() {
+        return pDescript;
+    }
+
+    public void setpDescript(String pDescript) {
+        this.pDescript = pDescript;
+    }
+
+    public String getpCategory() {
+        return pCategory;
+    }
+
+    public void setpCategory(String pCategory) {
+        this.pCategory = pCategory;
+    }
+
+    public Long getNetPrice() {
+        return netPrice;
+    }
+
+    public void setNetPrice(Long netPrice) {
+        this.netPrice = netPrice;
+    }
+
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,67 @@
+/*
+=============
+Navigation
+=============
+ */
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navContainer = document.querySelector(".nav__menu");
+
+/*
+=============
+PopUp
+=============
+ */
+const popup = document.querySelector(".popup");
+const closePopup = document.querySelector(".popup__close");
+
+if (popup) {
+  closePopup.addEventListener("click", () => {
+    popup.classList.add("hide__popup");
+  });
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      popup.classList.remove("hide__popup");
+    }, 500);
+  });
+}
+
+/*
+=============
+Fixed Navigation
+=============
+ */
+
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+
+// Smooth Scroll
+Array.from(scrollLink).map(link => {
+  link.addEventListener("click", e => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navBar.getBoundingClientRect().height;
+    const fixNav = navBar.classList.contains("fix__nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position - navHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    navContainer.style.left = "-30rem";
+    document.body.classList.remove("active");
+  });
+});
+
+// Fix NavBar
+
